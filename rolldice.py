e40e8e2e1c670deae63644d53c75198626d535fe
Leslie_Leung <lesily9@gmail,com>
2016-10-29 15:43:38 +0800

    the dice part of the game

---
+++ rolldice.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+#-*- coding=utf-8 -*-
+
+# the rolldice part for monopoly
+
+import math
+import random
+
+
+#def roll_dice():
+#	roll = random.randint(1,6)
+
+roll = random.randint(1,6)
+
+print "the number is", roll
